0ca07346c4cac2e8d8d81dd1ca68a04959b8810d
Valentin <valentin.morardo@gmail.com>
2023-08-16 17:09:35 -0300

    update dependencies

--- src/config/server.cfg.ts
+++ src/config/server.cfg.ts
@@ -1,6 +1,7 @@
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import express from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 const server = express();
 
@@ -12,21 +13,21 @@ server.use(cookieParser());
 // cargamos body parser que es un middleware para analizar cuerpos atravez de la url
 server.use(bodyParser.urlencoded({ extended: false }));
 // activamos el CORS para permitir las peticions AJAX y HTTP desde el front
-server.use((req, res, next) => {
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header(
+server.use((req: Request, res: Response, next: NextFunction) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
 		'Access-Control-Allow-Headers',
 		'Access-Control-Allow-Headers, Authorization, X-API-KEY, Origin, X-Requested-With, Content-type, Accept, Access-Control-Request-Method, Access-Control-Allow-Request-Method, Access-Control-Request-Headers,  Access-Control-Allow-Origin',
 	);
-	res.header('Access-Control-Allow-Headers', '*');
-	res.header('Access-Control-Request-Headers', '*');
-	res.header('Access-Control-Allow-Origin', '*');
-	res.header('Access-Control-Allow-Credentials', 'true');
-	res.header(
+	res.setHeader('Access-Control-Allow-Headers', '*');
+	res.setHeader('Access-Control-Request-Headers', '*');
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Credentials', 'true');
+	res.setHeader(
 		'Access-Control-Allow-Methods',
 		'GET, HEAD, POST, OPTIONS, PUT, DELETE',
 	);
-	res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+	res.setHeader('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
 	next();
 });
 
