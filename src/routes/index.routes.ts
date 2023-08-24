8b7bad2e28996cc80817248ef25ca0c996a86b53
Valentin <valentin.morardo@gmail.com>
2023-08-24 16:44:06 -0300

    routes for admin added

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -7,15 +7,17 @@ router.get('/', (req, res) => {
 	res.json({ message: 'Welcome to my API' });
 });
 
-
-
 router.post('/register' /* controller.register */);
 router.post('/login' /* controller.login */);
 router.get('/profile' /* controller.profile */);
 
-router.post('/profile/postNewGame', /* controller.postNewGame */)
-router.get('/profile/getAllGames', /* controller.getAllGames */);
-router.delete('/profile/deleteAGame/:id', /* controller.deleteAGame */);
-router.patch('/profile/patchAGame/:id', /* controller.patchAGame */);
+router.post('/profile/postNewGame' /* controller.postNewGame */);
+router.get('/profile/getAllGames' /* controller.getAllGames */);
+router.delete('/profile/deleteAGame/:id' /* controller.deleteAGame */);
+router.patch('/profile/patchAGame/:id' /* controller.patchAGame */);
+
+router.get('/admin/getAllProfiles' /* controller.getAllProfiles */);
+router.delete('/admin/deleteProfile' /* controller.deleteProfile */);
+router.post('/admin/newAdmin' /* controller.newAdmin */);
 
 export default router;
