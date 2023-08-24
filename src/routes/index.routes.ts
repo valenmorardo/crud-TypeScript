66a52ad619df4f8315683d394384bd7ab4fb2a44
Valentin <valentin.morardo@gmail.com>
2023-08-24 16:38:32 -0300

    new routes

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -7,9 +7,15 @@ router.get('/', (req, res) => {
 	res.json({ message: 'Welcome to my API' });
 });
 
-/* router.get('/getAllGames', controller.getAllGames);
-router.post('/postNewGame', controller.postNewGame);
-router.delete('/deleteAGame/:id', controller.deleteAGame);
-router.patch('/patchAGame/:id', controller.patchAGame); */
+
+
+router.post('/register' /* controller.register */);
+router.post('/login' /* controller.login */);
+router.get('/profile' /* controller.profile */);
+
+router.post('/profile/postNewGame', /* controller.postNewGame */)
+router.get('/profile/getAllGames', /* controller.getAllGames */);
+router.delete('/profile/deleteAGame/:id', /* controller.deleteAGame */);
+router.patch('/profile/patchAGame/:id', /* controller.patchAGame */);
 
 export default router;
