2967a8b75b8820dd3d157481e1dd04a339be5d02
Valentin <valentin.morardo@gmail.com>
2023-08-25 17:56:29 -0300

    login user controller function

---
+++ src/controllers/login-user/login-user.controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+import { httpStatusCodes } from "@libs/httpStatusCodes";
+
+import User_Model from "@models/User";
+
+export async function loginUser(req: Request, res: Response): Promise<void> {
+
+
+}
