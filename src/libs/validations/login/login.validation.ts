067d3edcf8985b0af1f36e0e8d1a6fbd182d348a
Valentin <valentin.morardo@gmail.com>
2023-08-25 17:57:02 -0300

    validation of fields for login user

---
+++ src/libs/validations/login/login.validation.ts
@@ -0,0 +1,33 @@
+import { Request, Response, NextFunction } from 'express';
+import { httpStatusCodes } from '@libs/httpStatusCodes';
+import validator from 'validator';
+
+export const validateLogin = (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	const { email, password } = req.body;
+
+	if (!validator.isEmail(email)) {
+		return res.status(400).send({
+			error_message: 'El email no esta en formato correcto.',
+			status: {
+				code: 400,
+				message: httpStatusCodes[400],
+			},
+		});
+	}
+
+	if (!validator.isEmpty(password)) {
+		return res.status(400).send({
+			error_message: 'la contraseña es obligatoria',
+			status: {
+				code: 400,
+				message: httpStatusCodes[400],
+			},
+		});
+	}
+
+	return next();
+};
