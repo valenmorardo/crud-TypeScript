a50d9d0a4f25b00ad8a4fd69cebfd983a392620e
Valentin <valentin.morardo@gmail.com>
2023-08-28 21:42:14 -0300

    refactor and documentatrion index.routes.ts

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,28 +1,56 @@
 import * as controllers from '@controllers/allControllers';
-import * as validations from '@libs/validations/allValidations'
-import * as middlewares from '@middlewares/allMiddlewares.routes'
+import * as validations from '@libs/validations/allValidations';
+import * as middlewares from '@middlewares/allMiddlewares.routes';
 
 import { Router } from 'express';
 const router: Router = Router();
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+//main route ---> /api
+//
+//~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 router.get('/', (_req, res) => {
 	res.json({ message: 'Welcome to my videogames CRUD API' });
 });
 
-router.post('/register', validations.validateRegisterFields, validations.userAlreadyRegistered, middlewares.encryptPassword, controllers.registerNewUser);
-router.post('/login', validations.validateLoginFields, validations.validateLoginData, controllers.loginUser);
+// login & register
+router.post(
+	'/register',
+	validations.validateRegisterFields,
+	validations.userAlreadyRegistered,
+	middlewares.encryptPassword,
+	controllers.registerNewUser,
+);
+router.post(
+	'/login',
+	validations.validateLoginFields,
+	validations.validateLoginData,
+	controllers.loginUser,
+);
+//~~~~~~~~~
 
-router.get('/allGames')
+// Profile routes
+router.use('/profile' /* middlewares.verifyAuthToken */);
 
 router.get('/profile' /* controller.profile */);
-router.get('/profile/myGames')
+router.get('/profile/myGames');
 router.delete('/profile/myGames/deleteAGame/:id' /* controller.deleteAGame */);
 router.patch('/profile/myGames/patchAGame/:id' /* controller.patchAGame */);
 router.post('/profile/postNewGame' /* controller.postNewGame */);
+//~~~~~~~~~
+
+// home // all games
+router.get('/allGames');
+//~~~~~~~~~
+
+// Admin routes
+router.use('/admin', /* middlewares.verifyAuthToken, */ /* middlewares.verifyIsAdmin */);
 
 router.get('/admin/getAllProfiles' /* controller.getAllProfiles */);
 router.delete('/admin/deleteProfile' /* controller.deleteProfile */);
 router.post('/admin/newAdmin' /* controller.newAdmin */);
+//~~~~~~~~~
 
 export default router;
