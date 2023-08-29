5dab3353277520f4eefc4d1548f26645b39de139
Valentin <valentin.morardo@gmail.com>
2023-08-28 21:24:38 -0300

    .catch added for errors

--- src/libs/validations/register/user-already-registered.ts
+++ src/libs/validations/register/user-already-registered.ts
@@ -11,7 +11,7 @@ export const userAlreadyRegistered = async (
 ) => {
 	const email_body: string = req.body.email;
 
-	const userFounded: boolean = await User_Model.findOne({
+	const user: boolean | void= await User_Model.findOne({
 		where: { email: email_body },
 	}).then((user) => {
 		if (!user) {
@@ -19,9 +19,15 @@ export const userAlreadyRegistered = async (
 		} else {
 			return true;
 		}
-	});
+	}).catch((err) => {
+		console.log(err);
+		res.status(500).send({
+			code:500,
+			msg: httpStatusCodes[500]
+		})
+	})
 
-	if (userFounded) {
+	if (user) {
 		return res.status(400).send({
 			message: 'Email already exists. Try to log in',
 			status: {
