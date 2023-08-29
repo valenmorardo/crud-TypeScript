a87d97e4be375ac21807f3020814df209e1970a8
Valentin <valentin.morardo@gmail.com>
2023-08-28 21:22:08 -0300

    jwt scret token added

--- src/config/var-environments.ts
+++ src/config/var-environments.ts
@@ -8,6 +8,8 @@ const env = {
   USERNAME_DB: process.env.USERNAME_DB,
   PASSWORD_DB: process.env.PASSWORD_DB,
   HOST: process.env.HOST,
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN
   
 };
 
