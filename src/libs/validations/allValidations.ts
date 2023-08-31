8a7b53ef52758863a5cbf29fee76df4f35198998
Valentin <valentin.morardo@gmail.com>
2023-08-31 20:52:00 -0300

    validation fields of body in NewGame post route ready

--- src/libs/validations/allValidations.ts
+++ src/libs/validations/allValidations.ts
@@ -1,14 +1,18 @@
-//register stage
+//register - stage
 import { validateRegisterFields } from './register/register-fields.validation';
 import { userAlreadyRegistered } from './register/user-already-registered.validation';
 
-// login stage
+// login - stage
 import { validateLoginFields } from './login/login-fields.validation';
 import { validateLoginData } from './login/login-data.validation';
+
+//post new gane - stage
+import { validateNewGameData } from './newGame/newGame-fields.validation';
 
 export {
 	validateRegisterFields,
 	validateLoginFields,
 	validateLoginData,
 	userAlreadyRegistered,
+	validateNewGameData
 };
---
+++ src/libs/validations/newGame/newGame-fields.validation.ts
@@ -0,0 +1,76 @@
+import { Request, Response, NextFunction } from 'express';
+import validator from 'validator';
+import { httpStatusCodes } from '@libs/httpStatusCodes';
+
+export const validateNewGameData = (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+): Response | void => {
+	const { name, description, price, genres } = req.body;
+
+	if (!validator.isAscii(name.trim())) {
+		return res.status(400).send({
+			message:
+				'El nombre del vidoejuego solo debe contener letra y/o numeros. Y es un campo obligatorio',
+			status: {
+				code: 400,
+				msg: httpStatusCodes[400],
+			},
+		});
+	}
+
+	if (!validator.isAscii(description.trim())) {
+		return res.status(400).send({
+			message:
+				'La descripcion del vidoejuego solo debe contener letra y/o numeros. Y es un campo obligatorio',
+			status: {
+				code: 400,
+				msg: httpStatusCodes[400],
+			},
+		});
+	}
+
+	if (!validator.isInt(price.toString())) {
+		return res.status(400).send({
+			message:
+				'El precio debe ser indicado en formato numerico. Y es un campo obligatorio',
+			status: {
+				code: 400,
+				msg: httpStatusCodes[400],
+			},
+		});
+	}
+
+	if (price <= 0) {
+		return res.status(400).send({
+			message: 'El precio minimo es de $1',
+			status: {
+				code: 400,
+				msg: httpStatusCodes[400],
+			},
+		});
+	}
+
+	if (!Array.isArray(genres)) {
+		return res.status(400).send({
+			message: 'EL dato recibido no cumple con el formato correcto.',
+			status: {
+				code: 400,
+				msg: httpStatusCodes[400],
+			},
+		});
+	}
+
+	if (genres.length === 0) {
+		return res.status(400).send({
+			message: 'Debe escojer minimo un genero.',
+			status: {
+				code: 400,
+				msg: httpStatusCodes[400],
+			},
+		});
+	}
+
+	return next();
+};
