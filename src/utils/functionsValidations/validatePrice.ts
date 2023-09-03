918e6283cca7c14e740937c0f24d90e6e1f48dd1
Valentin <valentin.morardo@gmail.com>
2023-09-03 20:50:29 -0300

    validate price ready

---
+++ src/utils/functionsValidations/validatePrice.ts
@@ -0,0 +1,18 @@
+import validator from 'validator';
+import { CustomError } from '../customError';
+
+export const validatePrice = (price: number): boolean => {
+	if (!validator.isEmpty(price.toString()))
+		throw new CustomError('El precio es obligatorio.', 400);
+
+	if (
+		!validator.isInt(price.toString()) ||
+		!validator.isFloat(price.toString())
+	) {
+		throw new CustomError(
+			'El numero proporcionado no es valido. Debe ser un entero o decimal.',
+			400,
+		);
+	}
+	return true;
+};
