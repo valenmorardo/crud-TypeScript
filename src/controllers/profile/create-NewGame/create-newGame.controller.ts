113599dd532fcbe3133cc4904dd429c9eeef33ed
Valentin <valentin.morardo@gmail.com>
2023-09-02 21:26:13 -0300

    erorr handling in new game controller changed

--- src/controllers/profile/create-NewGame/create-newGame.controller.ts
+++ src/controllers/profile/create-NewGame/create-newGame.controller.ts
@@ -2,29 +2,42 @@ import { Request, Response, NextFunction } from 'express';
 import { httpStatusCodes } from '@libs/httpStatusCodes';
 import { IVideogameAttributes } from '@libs/Types_&_Interfaces/videogame-attributes';
 import Videogame_Model from '@models/Videogames';
+import { CustomError } from 'src/utils/custom-error';
 
 export const createNewGame = async (
 	req: Request,
 	res: Response,
-	_next: NextFunction,
-): Promise<Response> => {
+	next: NextFunction,
+): Promise<Response | void> => {
     const {name, description, price, genres}: IVideogameAttributes = req.body
     const userId = req.userId
 
-    const newGame = await Videogame_Model.create({
-        name,
-        description,
-        price,
-        genres,
-        ownerUserId: userId
-    });
+    try {
+        const newGame = await Videogame_Model.create({
+            name,
+            description,
+            price,
+            genres,
+            ownerUserId: userId
+        });
+        
+        return res.status(201).send({
+            message: "Juego creado",
+            newGame,
+            status: {
+                code:201,
+                msg: httpStatusCodes[201]
+            }
+        })
 
-    return res.status(201).send({
-        message: "Juego creado",
-        newGame,
-        status: {
-            code:201,
-            msg: httpStatusCodes[201]
-        }
-    })
+
+    } catch (error: any) {
+        
+        error.error_message = error.message
+        error.message = 'Fallo al crearse el juego'
+
+        return next(error)
+    }
+
+
 };
