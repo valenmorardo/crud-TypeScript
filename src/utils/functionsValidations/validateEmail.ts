d4cce355ecd9d778fa9ae638feafa5dbe710a64d
Valentin <valentin.morardo@gmail.com>
2023-09-03 20:26:09 -0300

    validate email added validator.isEmpty

--- src/utils/functionsValidations/validateEmail.ts
+++ src/utils/functionsValidations/validateEmail.ts
@@ -1,8 +1,10 @@
 import validator from "validator";
+import { CustomError } from "../customError";
 
 export const validateEmail = (email: string): boolean => {
+    if(validator.isEmpty(email)) throw new CustomError('El email es obligatorio.', 400)
 
-    if(!validator.isEmail(email)) throw new Error("El email no es valido.")
+    if(!validator.isEmail(email)) throw new CustomError("El email no es valido.", 400)
 
     
     return true;
