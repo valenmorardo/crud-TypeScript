b38437cfea51ec0fcbd48f2a89bf52b56209b748
Valentin <valentin.morardo@gmail.com>
2023-09-03 20:37:59 -0300

    validate is uuid ready

--- src/utils/functionsValidations/validateIsUUID.ts
+++ src/utils/functionsValidations/validateIsUUID.ts
@@ -2,6 +2,7 @@ import validator from 'validator';
 import { CustomError } from '../customError';
 
 export const validateIsUUID = (id: string): boolean => {
+	if(validator.isEmpty(id)) throw new CustomError('La ID no puede ser vacio.')
 	if (!validator.isUUID(id)) throw new CustomError('No existe ese ID', 400);
 
 	return true;
