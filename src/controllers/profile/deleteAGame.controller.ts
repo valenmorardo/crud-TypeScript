b702e186a0dbf48741cd804e00a63be51860ef3b
Valentin <valentin.morardo@gmail.com>
2023-09-04 20:49:53 -0300

    delte game controller ready

--- src/controllers/profile/deleteAGame.controller.ts
+++ src/controllers/profile/deleteAGame.controller.ts
@@ -2,38 +2,35 @@ import { Request, Response, NextFunction } from 'express';
 
 import Videogame_Model from '@models/Videogames';
 import { httpStatusCodes } from '@libs/httpStatusCodes';
+import { CustomError } from '@utils/customError';
 
-export const deleteAGame = async (
+export const deleteAGame = (
 	req: Request,
 	res: Response,
-	_next: NextFunction,
-): Promise<Response> => {
+	next: NextFunction,
+): Response | void => {
 	const gameId = req.params.id;
 	const userId = req.userId;
 
-	const gameToDelete = await Videogame_Model.findOne({
+	Videogame_Model.findOne({
 		where: {
 			id: gameId,
 			ownerUserId: userId,
 		},
-	});
+	})
+		.then((game) => {
+			if (!game)
+				throw new CustomError('No posee ningun videojuego con ese ID.', 400);
 
-	if (!gameToDelete) {
-		return res.status(404).send({
-			message: 'No posee un juego con ese ID.',
-			status: {
-				code: 404,
-				msg: httpStatusCodes[400],
-			},
+			game.destroy();
+			return res.status(201).send({
+				game_deleted: true,
+				msg: 'Videojuego eliminado correctamente.',
+			});
+		})
+		.catch((error) => {
+			error.error_message = error.message;
+			error.mesage = 'Fallo a la hora de eliminar videojuego';
+			return next(error);
 		});
-	} else {
-		gameToDelete.destroy();
-		return res.status(200).send({
-			message: 'videojuego eliminado!',
-			status: {
-				code: 200,
-				msg: httpStatusCodes[200],
-			},
-		});
-	}
 };
