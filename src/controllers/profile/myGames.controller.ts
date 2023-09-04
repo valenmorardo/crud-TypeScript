26fd1d62d5820541442dace9aa6695fffbac5e2c
Valentin <valentin.morardo@gmail.com>
2023-09-04 20:36:51 -0300

    myGames controller ready

--- src/controllers/profile/myGames.controller.ts
+++ src/controllers/profile/myGames.controller.ts
@@ -2,43 +2,41 @@ import { Request, Response, NextFunction } from 'express';
 import { httpStatusCodes } from '@libs/httpStatusCodes';
 
 import Videogame_Model from '@models/Videogames';
+import { CustomError } from '@utils/customError';
 
 export const getMyGames = async (
 	req: Request,
 	res: Response,
 	next: NextFunction,
-): Promise<Response>=> {
+) => {
 	const id = req.userId;
 
-	const games = await Videogame_Model.findAll({
+	Videogame_Model.findAll({
 		where: {
 			ownerUserId: id,
 		},
 		attributes: {
 			exclude: ['ownerUserId'],
 		},
-	});
+	})
+		.then((games) => {
+			console.log(games)
+			if (!games.length)
+				throw new CustomError('No se encontraron juegos de su propiedad.', 400);
 
-
-	if (!games.length) {
-		return res.status(404).send({
-            message: 'No games found',
-            status: {
-                code: 404,
-                msg: httpStatusCodes[404]
-            }
-        })
-	}
-
-	const gamesFounded = games.map((juego) => {
-		return juego;
-	});
-
-	return res.status(200).send({
-		Your_Games: gamesFounded,
-		status: {
-			code: 200,
-			msg: httpStatusCodes[200],
-		},
-	});
+				const videogames = games.map((game) => {
+					return game.dataValues
+				})
+		
+			return res.status(200).send({
+				games_founded: true,
+				videogames,
+			
+			})
+		})
+		.catch((error) => {
+			error.error_message = error.message;
+			error.message = 'Fallo a la hora de obtener videojuegos'
+			return next(error)
+		});
 };
