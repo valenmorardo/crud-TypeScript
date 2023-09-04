550b1ea6370f026a6ba61ba326584058a72750d0
Valentin <valentin.morardo@gmail.com>
2023-09-04 18:27:44 -0300

    profile controller updated and finished

--- src/controllers/profile/profile.controller.ts
+++ src/controllers/profile/profile.controller.ts
@@ -1,31 +1,28 @@
-import { Request, Response } from 'express';
-import { httpStatusCodes } from '@libs/httpStatusCodes';
+import { NextFunction, Request, Response } from 'express';
 
 import User_Model from '@models/User';
 
-export const profileUser = (req: Request, res: Response): Response | void => {
-    const id = req.userId
+export const profileUser = (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+): Response | void => {
+	const id = req.userId;
 
-    User_Model.findByPk(id, {
-        attributes: {
-            exclude: ['password', 'isAdmin']
-        }
-    }).then((user) => {
-        return res.status(200).send({
-            user_profile: user,
-            status: {
-                code: 200,
-                msg: httpStatusCodes[200],
-            }
-        })
-    }).catch((err) => {
-        console.log(err);
-        return res.status(404).send({
-            message: "profile not founded. Try Again",
-            status: {
-                code:404,
-                msg: httpStatusCodes[400]
-            }
-        })
-    })
+	User_Model.findByPk(id, {
+		attributes: {
+			exclude: ['password', 'isAdmin'],
+		},
+	})
+		.then((user) => {
+			return res.status(200).send({
+				user_founded: true,
+				user_profile: user,
+			});
+		})
+		.catch((error) => {
+			error.error_message = error.message;
+			error.message = 'Fallo al a hora de obtener datos del perfil';
+			return next(error);
+		});
 };
