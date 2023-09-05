2d258651a4038348cf25e462a88fe43c77c65915
Valentin <valentin.morardo@gmail.com>
2023-09-04 21:02:16 -0300

    verify is admin ready

--- src/middlewares/validations/admin/verifyIsAdmin.ts
+++ src/middlewares/validations/admin/verifyIsAdmin.ts
@@ -2,25 +2,23 @@ import { Request, Response, NextFunction } from 'express';
 import { httpStatusCodes } from '@libs/httpStatusCodes';
 
 import User_Model from '@models/User';
+import { CustomError } from '@utils/customError';
 
-export const verifyIsAdmin = async (
+export const verifyIsAdmin =  (
 	req: Request,
 	res: Response,
 	next: NextFunction,
-): Promise<Response | void> => {
+): Response | void => {
 	const userId = req.userId;
 
-	const user = await User_Model.findByPk(userId);
+	User_Model.findByPk(userId).then((user) => {
+		if(!user?.dataValues.isAdmin) throw new CustomError('You Dont have permisson to access here.', 400);
+		return next()
+	}).catch((error) => {
+		error.error_message = error.message;
+		error.message = "Access Denied."
+		return next(error)
+	})
 
-	if (!user?.dataValues.isAdmin) {
-		return res.status(401).send({
-			message: 'Acces denied.',
-			status: {
-				code: 401,
-				msg: httpStatusCodes[401],
-			},
-		});
-	}
-
-	return next();
+	
 };
