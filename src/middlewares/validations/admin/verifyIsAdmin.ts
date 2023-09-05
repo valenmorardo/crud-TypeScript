0f7b42a5533c05bec73d5546d7ba22ec8a6f1e57
Valentin <valentin.morardo@gmail.com>
2023-09-04 21:06:39 -0300

    verify is admin middleware ready

--- src/middlewares/validations/admin/verifyIsAdmin.ts
+++ src/middlewares/validations/admin/verifyIsAdmin.ts
@@ -4,21 +4,22 @@ import { httpStatusCodes } from '@libs/httpStatusCodes';
 import User_Model from '@models/User';
 import { CustomError } from '@utils/customError';
 
-export const verifyIsAdmin =  (
+export const verifyIsAdmin = (
 	req: Request,
 	res: Response,
 	next: NextFunction,
 ): Response | void => {
 	const userId = req.userId;
 
-	User_Model.findByPk(userId).then((user) => {
-		if(!user?.dataValues.isAdmin) throw new CustomError('You Dont have permisson to access here.', 400);
-		return next()
-	}).catch((error) => {
-		error.error_message = error.message;
-		error.message = "Access Denied."
-		return next(error)
-	})
-
-	
+	User_Model.findByPk(userId)
+		.then((user) => {
+			if (!user?.dataValues.isAdmin)
+				throw new CustomError('You Dont have permisson to access here.', 400);
+			return next();
+		})
+		.catch((error) => {
+			error.error_message = error.message;
+			error.message = 'Access Denied.';
+			return next(error);
+		});
 };
