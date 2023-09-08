1fdbc69ec813b2770c20ac5a6c3c78190025e8a4
Valentin <valentin.morardo@gmail.com>
2023-09-07 21:38:51 -0300

    relationships added

--- src/models/Admin.ts
+++ src/models/Admin.ts
@@ -2,29 +2,22 @@ import { DataTypes, Model } from 'sequelize';
 import Database from '@config/database';
 import { IAdminAttributes } from '@libs/typings/adminAttributes';
 
-const Admin_Model = Database.define<Model, IAdminAttributes>(
+const Admin_Model = Database.define<Model>(
 	'admin',
 	{
 		id: {
-			type: DataTypes.UUIDV4,
+			type: DataTypes.UUID,
 			allowNull: false,
 			primaryKey: true,
 			unique: true,
 		},
-		name: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		email: {
-			type: DataTypes.STRING,
-			unique: true,
-			allowNull: false,
-		},
+
 	},
 	{
 		timestamps: false,
 	},
 );
+
 
 
 export default Admin_Model;
