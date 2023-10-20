404d2d2de06806c28ffda0fddcbff38b42e6603d
Valentin <valentin.morardo@gmail.com>
2023-10-20 20:12:32 -0300

    get api test added

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,15 +1,28 @@
-import request from 'supertest'
-import server from '../src/config/server.cfg.ts'
+import request from 'supertest';
+import server from '../src/config/server.cfg.ts';
 
 describe('GET /', () => {
-    test('should return status code 200', async () => {
-        const response = await request(server).get('/').send();
-        console.log(response)
-        expect(response.statusCode).toBe(200)
-    })
-    
-    test('el body debe ser text/html', async () => {
-        const response = await request(server).get('/').send();
-        expect(response.header['content-type']).toMatch('html');
-    })
-}) 
+	test('should return status code 200', async () => {
+		const response = await request(server).get('/').send();
+		console.log(response);
+		expect(response.statusCode).toBe(200);
+	});
+
+	test('el body debe ser text/html', async () => {
+		const response = await request(server).get('/').send();
+		expect(response.header['content-type']).toMatch('html');
+	});
+});
+
+describe('GET /api', () => {
+	test('should respond with status code 200', (done) => {
+		request(server)
+			.get('/api')
+			.send()
+			.expect(200)
+			.end((err) => {
+                if(err) done(err);
+                done()
+            });
+	});
+});
