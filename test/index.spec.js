16b45774813eadb60f5502b030ce1392b3e7a5e8
Valentin <valentin.morardo@gmail.com>
2023-10-20 18:13:08 -0300

    test para comprobar q el content type de / sea text html

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,6 +4,12 @@ import server from '../src/config/server.cfg.ts'
 describe('GET /', () => {
     test('should return status code 200', async () => {
         const response = await request(server).get('/').send();
+        console.log(response)
         expect(response.statusCode).toBe(200)
+    })
+    
+    test('el body debe ser text/html', async () => {
+        const response = await request(server).get('/').send();
+        expect(response.header['content-type']).toMatch('html');
     })
 }) 
